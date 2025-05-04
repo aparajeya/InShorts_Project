e221bd78793dc262c74821e0ad322e2c8a37d425
Aparajeya <akashaparajeya@gmail.com>
2025-05-04 18:37:33 +0530

    Changed settings

--- InShorts_Project/settings.py
+++ InShorts_Project/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -69,18 +70,11 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = "InShorts_Project.wsgi.application"
+ALLOWED_HOSTS = ['InShortsProject.onrender.com']
 
 
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
-
-"""
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
-}
 """
 DATABASES = {
     "default": {
@@ -92,9 +86,25 @@ DATABASES = {
         },
     }
 }
+"""
+# TODO MOVE THESE VALUES AND API KEYS TO .env FILE
 
-MONGO_URI = "mongodb://localhost:27017/"
-MONGO_DB = "InshortsDB"
+DATABASES = {
+    'default': {
+        'ENGINE': 'djongo',
+        'NAME': 'InshortsDB',  
+        'ENFORCE_SCHEMA': False,
+        'CLIENT': {
+            'host': 'mongodb+srv://lonefighter4747:akash123@inshortsdb.ggcua65.mongodb.net/InshortsDB?retryWrites=true&w=majority',  # MongoDB URI
+            'username': 'lonefighter4747', 
+            'password': 'akash123',  
+        }
+    }
+}
+
+
+#MONGO_URI = "mongodb://localhost:27017/"
+#MONGO_DB = "InshortsDB"
 
 
 # Password validation
